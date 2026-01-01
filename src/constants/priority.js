a95a1038d173b1d8ec73a33d67c313e230a96228
chetracoding <hongchetra12@gmail.com>
2026-01-02 00:33:52 +0700

    feat(report): update language

--- src/constants/priority.js
+++ src/constants/priority.js
@@ -8,6 +8,6 @@ export const PRIORITY_NAME = {
 export const PRIORITY_OPTIONS = [
   { title: 'report.priority.critical', value: PRIORITY_NAME.CRITICAL },
   { title: 'report.priority.high', value: PRIORITY_NAME.HIGH },
-  { title: 'report.priority.midium', value: PRIORITY_NAME.MEDIUM },
+  { title: 'report.priority.medium', value: PRIORITY_NAME.MEDIUM },
   { title: 'report.priority.low', value: PRIORITY_NAME.LOW },
 ]
